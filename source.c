79909824f4442b2590e55d28ee2c5910e85438d6
hacker-test-github <117366666+hacker-test-github@users.noreply.github.com>
2023-01-05 19:02:56 +0200

    Update source.c

--- source.c
+++ source.c
@@ -1,68 +1,22 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    // it can be called REP if you want but "REPEAT" is clearer and still fits under the 8 char "limit"
-    REPEAT = SAFE_RANGE,
+    REP = SAFE_RANGE
 };
 
-// use REPEAT in the layout
+// use REP in the layout
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = ...
 
-// Used to extract the basic tapping keycode from a dual-role key.
-// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
-#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
-uint16_t last_keycode = KC_NO;
-uint8_t last_modifier = 0;
-
-// Initialize variables holding the bitfield
-// representation of active modifiers.
-uint8_t mod_state;
-uint8_t oneshot_mod_state;
-
-void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
-    if (keycode != REPEAT) {
-        // Early return when holding down a pure layer key
-        // to retain modifiers
-        switch (keycode) {
-            case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
-            case QK_MOMENTARY ... QK_MOMENTARY_MAX:
-            case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
-            case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
-            case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
-            case QK_TO ... QK_TO_MAX:
-            case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
-                return;
-        }
-        last_modifier = oneshot_mod_state > mod_state ? oneshot_mod_state : mod_state;
-        switch (keycode) {
-            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-                if (record->event.pressed) {
-                    last_keycode = GET_TAP_KC(keycode);
-                }
-                break;
-            default:
-                if (record->event.pressed) {
-                    last_keycode = keycode;
-                }
-                break;
-        }
-    } else { // keycode == REPEAT
-        if (record->event.pressed) {
-            register_mods(last_modifier);
-            register_code16(last_keycode);
+uint16_t key = 0;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if(record->event.pressed) {
+        if(keycode == REP) {
+            tap_code(key);
+            return false;
         } else {
-            unregister_code16(last_keycode);
-            unregister_mods(last_modifier);
+            key = keycode;
+            return true;
         }
     }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    process_repeat_key(keycode, record);
-    // It's important to update the mod variables *after* calling process_repeat_key, or else
-    // only a single modifier from the previous key is repeated (e.g. Ctrl+Shift+T then Repeat produces Shift+T)
-    mod_state = get_mods();
-    oneshot_mod_state = get_oneshot_mods();
     return true;
 };
